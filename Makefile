dab68c858621220c0b00c847d20310bf17931487
ktvexe <ktvexe@gmail.com>
2018-02-02 14:09:54 +0800

    merge flush & non-flush

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
 ARM_CC ?= arm-linux-gnueabihf-gcc
 ARM_CFLAGS = -g -Wall -Wextra -Ofast -mfpu=neon
-EXEC = pipeline
+EXEC = pipeline pipeline_flush
 
 all:$(EXEC)
 
 pipeline: pipeline.c
-	$(ARM_CC) $(ARM_CFLAGS) -o $@ $<
+	$(ARM_CC) $(ARM_CFLAGS) -DNON_FLUSH -o $@ $<
+
+pipeline_flush: pipeline.c
+	$(ARM_CC) $(ARM_CFLAGS) -DFLUSH -o $@ $<
 
 clean:
-	$(RM) pipeline
+	$(RM) $(EXEC)
--- pipeline.c
+++ pipeline.c
@@ -5,14 +5,14 @@
 #include <arm_neon.h>
 
 //#define NUMBER_OF_INPUTS 4
+//#define SIZE 16
 #define SIZE 1000000
 
 long neon_iteration;
 
 float32_t mul(float *source,float *weight);
 
-static long diff_in_us(struct timespec t1, struct timespec t2) 
-{
+static long diff_in_us(struct timespec t1, struct timespec t2){
     struct timespec diff;
     if (t2.tv_nsec-t1.tv_nsec < 0) {
         diff.tv_sec  = t2.tv_sec - t1.tv_sec - 1;
@@ -30,15 +30,18 @@ int main(int argc, char *argv[]){
     struct timespec start,end;
     float source[SIZE] ;
     float weight[SIZE] ;
+    FILE *fptr;
     srand(time(NULL));
     for(int i = 0;i<SIZE;i++){
-        source[i] = ((float32_t)rand() /(float32_t)(RAND_MAX) ) * 100.0;
-        weight[i] = ((float32_t)rand() /(float32_t)(RAND_MAX) ) * 100.0;
+    //    source[i] = ((float32_t)rand() /(float32_t)(RAND_MAX) ) * 100.0;
+    //    weight[i] = ((float32_t)rand() /(float32_t)(RAND_MAX) ) * 100.0;
+        source[i] = (float32_t)(rand()%100)+1;
+        weight[i] = (float32_t)(rand()%100)+1;
     }
     clock_gettime(CLOCK_REALTIME,&start);
-    printf("output: %f\n",mul(source,weight));
+    printf("output:  %f\n",mul(source,weight));
     clock_gettime(CLOCK_REALTIME,&end);
-    printf("spend: %ld us\n",diff_in_us(start,end));
+    printf("spend:  %ld us\n",diff_in_us(start,end));
     return 0;
 }
 
@@ -52,14 +55,22 @@ float32_t mul(float *source,float *weights){
         in1_128 = vld1q_f32(&source[i]);
         in2_128 = vld1q_f32(&weights[i]);
         prod = vmulq_f32(in1_128, in2_128);
+#ifdef FLUSH
         sum1 = vaddq_f32(prod, vrev64q_f32(prod));
         sum2 = vaddq_f32(sum1, vcombine_f32(vget_high_f32(sum1), vget_low_f32(sum1)));
         vst1q_f32((float32_t *)result , sum2);
         output += result[0];
+#endif
     }
-    printf("loop times: %d\n",i);
+    //printf("loop times: %d\n",i);
+#ifdef NON_FLUSH
+    sum1 = vaddq_f32(prod, vrev64q_f32(prod));
+    sum2 = vaddq_f32(sum1, vcombine_f32(vget_high_f32(sum1), vget_low_f32(sum1)));
+    vst1q_f32((float32_t *)result , sum2);
+    output = result[0];
+#endif
 
     return output;
-
-
 }
+
+
