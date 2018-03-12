d06589fe7f8af72fec5ba9eec5c895e465b54597
ktvexe <ktvexe@gmail.com>
2018-03-12 15:08:13 +0800

    Fix unclosing file pointer

--- pipeline.c
+++ pipeline.c
@@ -13,7 +13,8 @@ long neon_iteration;
 float32_t mul(float *source,float *weight);
 void create_input(int size);
 
-static long diff_in_us(struct timespec t1, struct timespec t2){
+static long diff_in_us(struct timespec t1, struct timespec t2)
+{
     struct timespec diff;
     if (t2.tv_nsec-t1.tv_nsec < 0) {
         diff.tv_sec  = t2.tv_sec - t1.tv_sec - 1;
@@ -21,12 +22,13 @@ static long diff_in_us(struct timespec t1, struct timespec t2){
     } else {
         diff.tv_sec  = t2.tv_sec - t1.tv_sec;
         diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
-    }   
+    }
     return (diff.tv_sec * 1000000.0 + diff.tv_nsec / 1000.0);
 }
 
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 
     struct timespec start,end;
     float source[SIZE] ;
@@ -36,31 +38,32 @@ int main(int argc, char *argv[]){
     FILE *fptr;
     create_input(SIZE);
     fptr = fopen(FILE_NAME,"r");
-    for(int i = 0;i<SIZE;i++){
+    for(int i = 0; i<SIZE; i++) {
         fscanf(fptr,"%s %s %f %f\n",source_char,weight_char,&source[i],&weight[i]);
     }
     clock_gettime(CLOCK_REALTIME,&start);
     printf("output:  %f\n",mul(source,weight));
     clock_gettime(CLOCK_REALTIME,&end);
     printf("spend:  %ld us\n",diff_in_us(start,end));
+    fclose(fptr);
     return 0;
 }
 
-float32_t mul(float *source,float *weights){
+float32_t mul(float *source,float *weights)
+{
     float32x4_t in1_128,in2_128,sum1,sum2,prod;
     float32_t result[4];
     float32_t output = 0.0;
     int i;
     prod = vmovq_n_f32(0.0f);
-    for (i=0; i<SIZE; i+=4)
-    {
+    for (i=0; i<SIZE; i+=4) {
         in1_128 = vld1q_f32(&source[i]);
         in2_128 = vld1q_f32(&weights[i]);
 #ifdef FLUSH
         prod = vmulq_f32(in1_128, in2_128);
         sum1 = vaddq_f32(prod, vrev64q_f32(prod));
         sum2 = vaddq_f32(sum1, vcombine_f32(vget_high_f32(sum1), vget_low_f32(sum1)));
-        vst1q_f32((float32_t *)result , sum2);
+        vst1q_f32((float32_t *)result, sum2);
         output += result[0];
 #endif
 #ifdef NON_FLUSH
@@ -70,22 +73,23 @@ float32_t mul(float *source,float *weights){
 #ifdef NON_FLUSH
     sum1 = vaddq_f32(prod, vrev64q_f32(prod));
     sum2 = vaddq_f32(sum1, vcombine_f32(vget_high_f32(sum1), vget_low_f32(sum1)));
-    vst1q_f32((float32_t *)result , sum2);
+    vst1q_f32((float32_t *)result, sum2);
     output = result[0];
 #endif
 
     return output;
 }
 
-void create_input(int size){
+void create_input(int size)
+{
     FILE *fptr;
     if( access( FILE_NAME, F_OK ) != -1 ) {
         return;
-    } 
-    
+    }
+
     fptr = fopen(FILE_NAME,"w");
     srand(time(NULL));
-    for(int i = 0;i<size;i++){
+    for(int i = 0; i<size; i++) {
         fprintf(fptr,"source weight %f %f\n",(float32_t)(rand()%4)+1,(float32_t)(rand()%4)+1);
     }
     fclose(fptr);
